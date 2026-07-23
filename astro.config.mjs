e6e5c7c72074be4d999932ba9c6b4772b584cb9d
Emanuel <emanuelnaval07@gmail.com>
2026-07-23 10:08:59 -0500

    add - workflow for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  site: "https://naval07.github.io",
+  base: "psicologa_lilian_oviedo"
 });
